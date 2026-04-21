f5220d50dccaa7d8c3e6f9e95c040cfaa8112497
rodrigolinux10 <rodrigolinnux@gmail.com>
2026-04-21 14:09:58 -0300

    adicionando exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -20,10 +20,10 @@ import math
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
-raio = float(input("digitie o raio:"))
+"""raio = float(input("digitie o raio:"))
 area = math.pi * raio ** 2
 print(f"A área do círculo é: {area:.2f}")
-
+"""
 
 # #### Strings (`str`)
 
@@ -31,6 +31,13 @@ print(f"A área do círculo é: {area:.2f}")
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+data_do_usuario = input("Digite uma data no formato dd/mm/aaaa: ")
+lista_dia_mes_ano = data_do_usuario.split("/")
+print(f"Dia: {lista_dia_mes_ano[0]}")
+print(f"Mês: {lista_dia_mes_ano[1]}")
+print(f"Ano: {lista_dia_mes_ano[2]}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
